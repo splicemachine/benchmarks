8d004218069c99e9329a284239494116e4f48a1e
Murray Brown <mbrown@splicemachine.com>
2018-04-16 21:48:10 -0700

    Handle spark too

--- TPC-H/splicemachine/push-etc-hosts.sh
+++ TPC-H/splicemachine/push-etc-hosts.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
 FRAMEWORK=$1
-SPLICEHDFS=$2
+ENVARG=$2
 
 # TODO: better usage and help
 
-if [[ "$FRAMEWORK" == "" || "$SPLICEHDFS" == "" ]]; then
+if [[ "$FRAMEWORK" == "" || "$ENVARG" == "" ]]; then
   echo "Error: we need two arguments!"
   exit 1
 fi
+
+# get DOMAIN and SPLICEHDFS from ENVARG
+DOMAIN=$(dcos config show core.dcos_url)
+SPLICEHDFS=""
+ORIGENV=$DOMAIN
+if [[ "$ENVARG" == "d" || "$ENVARG" == "dev" ]]; then
+  DOMAIN="us-east-1.splicemachine-dev.io"
+  SPLICEHDFS="splicehdfs03"
+  if (( $VERBOSE )); then echo "switching to dev env $DOMAIN"; fi
+elif [[ "$ENVARG" == "q" || "$ENVARG" == "qa" ]]; then
+  DOMAIN="us-east-1.splicemachine-qa.io"
+  SPLICEHDFS="splicehdfs02"
+  if (( $VERBOSE )); then echo "switching to qa env $DOMAIN"; fi
+elif [[ "$ENVARG" == "p" || "$ENVARG" == "prod" || "$ENVARG" == "production" ]]; then
+  DOMAIN="us-east-1.splicemachine.io"
+  SPLICEHDFS="splicehdfs01"
+  if (( $VERBOSE )); then echo "switching to prod env $DOMAIN"; fi
+else
+  echo "Error: environment $ENVARG not recognized"
+  exit 2
+fi
+
+# ====
 
 # TODO: generate these lists automagicaly from dcos api
 
@@ -31,7 +54,14 @@ fi
 # dcos ... task exec -ti $task bash -c hostname
 # }
 
+# get framework_id from framework
+# e.g. dcos service | grep "murraysaccount-tpch1g10g100g-a0fb840a0b-spark" | awk '{print $8}'
+getFrameworkId() {
+  dcos service | grep "$FRAMEWORK-spark" | awk '{print $8}'
+}
+
 pods=( "hmaster-0" "hmaster-1" "hregion-0" "hregion-1" "hregion-2" "hregion-3" "zookeeper-0" "zookeeper-1" "zookeeper-2")
+#pods=( "hmaster-0" "hmaster-1")
 names=( "hmaster-0-node.${FRAMEWORK}.mesos" \
 	"hmaster-1-node.${FRAMEWORK}.mesos" \
 	"hregion-0-node.${FRAMEWORK}.mesos" \
@@ -43,7 +73,7 @@ names=( "hmaster-0-node.${FRAMEWORK}.mesos" \
 	"data-2-node.${SPLICEHDFS}.mesos" \
 	"data-3-node.${SPLICEHDFS}.mesos" )
 
-echo "Debug: pod-0 ${pods[0]} names-0 ${names[0]}"
+#echo "Debug: pod-0 ${pods[0]} names-0 ${names[0]}"
 
 # =====
 # Subroutines
@@ -71,7 +101,7 @@ pushDNS() {
   local file=$2
 
   echo push DNS $file to $task
-  cat $file | dcos task exec -i $task bash -c "cat >> /etc/hosts"
+  #cat $file | dcos task exec -i $task bash -c "cat >> /etc/hosts"
 }
 
 # ====
@@ -91,7 +121,7 @@ echo "found hmasterTask $hmasterTask"
 
 # loop through pods getting dns
 declare -i i=0
-for pod in ${pods[@]}; do
+for name in ${names[@]}; do
   echo "get ip for name ${names[$i]} using $hmasterTask"
   ip=$(fetchIpFromTask ${names[$i]} $hmasterTask | sed -e "s///g")
   echo -e "$ip\t\t${names[$i]}"  >> $DNS
@@ -104,11 +134,29 @@ echo -e "\n"
 
 # loop through pods, pushing DNS to tasks
 for pod in ${pods[@]}; do
-  echo "push dns to pod ${pod[0]}"
+  #echo "push dns to pod ${pod[0]}"
   task=$(findTask $pod)
-  #echo pushing etc hosts on $pod via task $task
+  echo pushing $DNS on $pod via task $task
   pushDNS $task $DNS
 done
 
-#cleanup
-#rm $DNS
+frameworkId=$(getFrameworkId $FRAMEWORK)  
+
+# TODO: now loop spark
+# e.g. curl -v -skSL -X GET -H "Content-Type:application/json"  
+# "https://us-east-1.splicemachine-dev.io/mesos/tasks | \
+# jq -er '.tasks[] | select(.framework_id=="5a9b24cc-8d57-46cd-88d7-c9927ff1ea35-0031") | \
+#  select(.state=="TASK_RUNNING") | .id'
+
+frameworkId=$(getFrameworkId $FRAMEWORK)  
+HEADER="-skSL -X GET -H \"Content-Type:application/json\""
+
+#echo curl ${HEADER} https://${DOMAIN}/mesos/tasks 
+for spark in $( curl ${HEADER} https://${DOMAIN}/mesos/tasks | jq -er --arg FRAME "$frameworkId" '.tasks[] | select(.framework_id==$FRAME) | select(.state=="TASK_RUNNING") | .id' ); do 
+   echo pushing $DNS to spark-task $spark
+   pushDNS $spark $DNS
+done
+
+
+echo cleanup $DNS
+rm $DNS
