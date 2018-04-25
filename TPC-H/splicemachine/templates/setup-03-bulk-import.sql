1c74ed3346ef476c9ae31b7aa646133f4a651411
Murray Brown <mbrown@splicemachine.com>
2018-04-25 11:53:36 -0700

    Adding both linear and bulk options to setup templates, capture timing of import

---
+++ TPC-H/splicemachine/templates/setup-03-bulk-import.sql
@@ -0,0 +1,11 @@
+SET SCHEMA ##SCHEMA##;
+elapsedtime on;
+
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'LINEITEM', null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/lineitem', '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'ORDERS',   null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/orders',   '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'CUSTOMER', null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/customer', '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'PARTSUPP', null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/partsupp', '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'SUPPLIER', null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/supplier', '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'PART',     null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/part',     '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'REGION',   null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/region',   '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
+call SYSCS_UTIL.BULK_IMPORT_HFILE ('##SCHEMA##', 'NATION',   null, 's3a://splice-benchmark-data/flat/TPCH/##SCALE##/nation',   '|', null, null, null, null, 0, '/tmp', true, null, '/tmp/##SCHEMA##_##SCALE##');
