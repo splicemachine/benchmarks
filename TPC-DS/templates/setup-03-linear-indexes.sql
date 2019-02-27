b1b0dc474801b7f140b2bf66ed940b17f3499881
splice <splice@stl-colo-srv112.splicemachine.colo>
2019-02-27 22:18:33 +0000

    query 10 optimization

--- TPC-DS/templates/setup-03-linear-indexes.sql
+++ TPC-DS/templates/setup-03-linear-indexes.sql
@@ -3,3 +3,6 @@ elapsedtime on;
 
 -- TODO: create any needed TPC-DS indexes here when loading data before creating indexes
 
+CREATE INDEX catalog_sales_customer_by_date ON catalog_sales(cs_sold_date_sk, cs_ship_customer_sk);
+CREATE INDEX web_sales_customer_by_date ON web_sales(ws_sold_date_sk, ws_bill_customer_sk);
+CREATE INDEX store_sales_customer_by_date ON store_sales(ss_sold_date_sk, ss_customer_sk);
