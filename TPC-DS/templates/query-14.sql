dc7b88d84fdcd94aecb15c3a1c0946f910f44f55
jramineni <jramineni@splicemachine.com>
2019-02-28 08:54:27 -0800

    TPCDS Query 14 : the with is doing intersect based on combination of brand, category and class, so moved this to where it is required with exists.

--- TPC-DS/templates/query-14.sql
+++ TPC-DS/templates/query-14.sql
@@ -1,43 +1,4 @@
-SET SCHEMA ##SCHEMA##;
-elapsedtime on;
--- TPC-DS QUERY 14
-with  cross_items as
- (select i_item_sk ss_item_sk
- from item,
- (select iss.i_brand_id brand_id
-     ,iss.i_class_id class_id
-     ,iss.i_category_id category_id
- from store_sales
-     ,item iss
-     ,date_dim d1
- where ss_item_sk = iss.i_item_sk
-   and ss_sold_date_sk = d1.d_date_sk
-   and d1.d_year between 1998 AND 1998 + 2
- intersect 
- select ics.i_brand_id brand_id
-     ,ics.i_class_id class_id 
-     ,ics.i_category_id category_id
- from catalog_sales
-     ,item ics
-     ,date_dim d2
- where cs_item_sk = ics.i_item_sk
-   and cs_sold_date_sk = d2.d_date_sk
-   and d2.d_year between 1998 AND 1998 + 2
- intersect
- select iws.i_brand_id brand_id
-     ,iws.i_class_id class_id
-     ,iws.i_category_id category_id
- from web_sales
-     ,item iws
-     ,date_dim d3
- where ws_item_sk = iws.i_item_sk
-   and ws_sold_date_sk = d3.d_date_sk
-   and d3.d_year between 1998 AND 1998 + 2) ss
- where i_brand_id = brand_id
-      and i_class_id = class_id
-      and i_category_id = category_id
-),
- avg_sales as
+with avg_sales as
  (select avg(quantity*list_price) average_sales
   from (select ss_quantity quantity
              ,ss_list_price list_price
@@ -45,13 +6,13 @@ with  cross_items as
            ,date_dim
        where ss_sold_date_sk = d_date_sk
          and d_year between 1998 and 1998 + 2
-       union all 
-       select cs_quantity quantity 
+       union all
+       select cs_quantity quantity
              ,cs_list_price list_price
        from catalog_sales
            ,date_dim
        where cs_sold_date_sk = d_date_sk
-         and d_year between 1998 and 1998 + 2 
+         and d_year between 1998 and 1998 + 2
        union all
        select ws_quantity quantity
              ,ws_list_price list_price
@@ -60,17 +21,17 @@ with  cross_items as
        where ws_sold_date_sk = d_date_sk
          and d_year between 1998 and 1998 + 2) x)
   select top 100 channel, i_brand_id,i_class_id,i_category_id,sum(sales), sum(number_sales)
- from(
+ from
+ (
        select 'store' channel, i_brand_id,i_class_id
              ,i_category_id,sum(ss_quantity*ss_list_price) sales
              , count(*) number_sales
        from store_sales
            ,item
            ,date_dim
-       where ss_item_sk in (select ss_item_sk from cross_items)
-         and ss_item_sk = i_item_sk
+       where ss_item_sk = i_item_sk
          and ss_sold_date_sk = d_date_sk
-         and d_year = 1998+2 
+         and d_year = 1998+2
          and d_moy = 11
        group by i_brand_id,i_class_id,i_category_id
        having sum(ss_quantity*ss_list_price) > (select average_sales from avg_sales)
@@ -79,10 +40,9 @@ with  cross_items as
        from catalog_sales
            ,item
            ,date_dim
-       where cs_item_sk in (select ss_item_sk from cross_items)
-         and cs_item_sk = i_item_sk
+       where cs_item_sk = i_item_sk
          and cs_sold_date_sk = d_date_sk
-         and d_year = 1998+2 
+         and d_year = 1998+2
          and d_moy = 11
        group by i_brand_id,i_class_id,i_category_id
        having sum(cs_quantity*cs_list_price) > (select average_sales from avg_sales)
@@ -91,14 +51,46 @@ with  cross_items as
        from web_sales
            ,item
            ,date_dim
-       where ws_item_sk in (select ss_item_sk from cross_items)
-         and ws_item_sk = i_item_sk
+       where ws_item_sk = i_item_sk
          and ws_sold_date_sk = d_date_sk
          and d_year = 1998+2
          and d_moy = 11
        group by i_brand_id,i_class_id,i_category_id
        having sum(ws_quantity*ws_list_price) > (select average_sales from avg_sales)
  ) y
- group by rollup (channel, i_brand_id,i_class_id,i_category_id)
+ where exists (select 1
+ 	from store_sales
+     ,item iss
+     ,date_dim d1
+ 	where ss_item_sk = iss.i_item_sk
+   and ss_sold_date_sk = d1.d_date_sk
+   and d1.d_year between 1998 AND 1998 + 2
+   and iss.i_brand_id = y.i_brand_id
+   and iss.i_class_id = y.i_class_id
+   and iss.i_category_id = y.i_category_id
+	)
+ and exists ( select 1
+ from catalog_sales
+     ,item ics
+     ,date_dim d2
+ where cs_item_sk = ics.i_item_sk
+   and cs_sold_date_sk = d2.d_date_sk
+   and d2.d_year between 1998 AND 1998 + 2
+   and ics.i_brand_id = y.i_brand_id
+   and ics.i_class_id = y.i_class_id
+   and ics.i_category_id = y.i_category_id
+ )
+ and exists ( select 1
+ from web_sales
+     ,item iws
+     ,date_dim d3
+ where ws_item_sk = iws.i_item_sk
+   and ws_sold_date_sk = d3.d_date_sk
+   and d3.d_year between 1998 AND 1998 + 2
+   and iws.i_brand_id = y.i_brand_id
+   and iws.i_class_id = y.i_class_id
+   and iws.i_category_id = y.i_category_id
+ )
+  group by rollup (channel, i_brand_id,i_class_id,i_category_id)
  order by channel,i_brand_id,i_class_id,i_category_id
  ;
