bf04b8393adc9319fc2106d3241785bd82c38a6c
jramineni <jramineni@splicemachine.com>
2019-02-27 16:20:41 -0800

    TPCDS Query 95 : The query runs fine, added index to hel the query for with clause, since that is referenced twice.

--- TPC-DS/templates/setup-03-linear-indexes.sql
+++ TPC-DS/templates/setup-03-linear-indexes.sql
@@ -3,3 +3,4 @@ elapsedtime on;
 
 -- TODO: create any needed TPC-DS indexes here when loading data before creating indexes
 
+create index web_sales_idx1 ON  web_sales (ws_order_number, ws_warehouse_sk);
