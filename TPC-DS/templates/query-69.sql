d9deb52d4cf78f5d8877d99d094d637b5b6e1714
yxia92 <yxia@splicemachine.com>
2019-02-27 16:04:48 -0800

    Rewrite query-69 by putting exists subquery and its outer table into a derived table helps to flatten the two not exists subqueries

--- TPC-DS/templates/query-69.sql
+++ TPC-DS/templates/query-69.sql
@@ -2,7 +2,9 @@ SET SCHEMA ##SCHEMA##;
 elapsedtime on;
 -- TPC-DS QUERY 69
 
-select top 100 
+/* put the exists subquery with its outer table into a derived table helps to flatten the not exists subquery */
+/* query that couldn't finish in 8.7 mins now can finish in ~40s */
+select top 100
   cd_gender,
   cd_marital_status,
   cd_education_status,
@@ -11,30 +13,34 @@ select top 100
   count(*) cnt2,
   cd_credit_rating,
   count(*) cnt3
- from
-  customer c,customer_address ca,customer_demographics
+from --splice-properties joinOrder=fixed
+(select * from 
+   (select * from customer c
+    where exists (select *
+                  from store_sales,date_dim
+                  where c.c_customer_sk = ss_customer_sk and
+                        ss_sold_date_sk = d_date_sk and
+                        d_year = 1999 and
+                        d_moy between 1 and 1+2)) c
  where
-  c.c_current_addr_sk = ca.ca_address_sk and
-  ca_state in ('CO','IL','MN') and
-  cd_demo_sk = c.c_current_cdemo_sk and 
-  exists (select *
-          from store_sales,date_dim
-          where c.c_customer_sk = ss_customer_sk and
-                ss_sold_date_sk = d_date_sk and
-                d_year = 1999 and
-                d_moy between 1 and 1+2) and
-   (not exists (select *
+ not exists (select *
             from web_sales,date_dim
             where c.c_customer_sk = ws_bill_customer_sk and
                   ws_sold_date_sk = d_date_sk and
                   d_year = 1999 and
-                  d_moy between 1 and 1+2) and
-    not exists (select * 
+                  d_moy between 1 and 1+2)
+ and 
+ not exists (select *
             from catalog_sales,date_dim
             where c.c_customer_sk = cs_ship_customer_sk and
                   cs_sold_date_sk = d_date_sk and
                   d_year = 1999 and
-                  d_moy between 1 and 1+2))
+                  d_moy between 1 and 1+2)
+) as C, customer_address ca,customer_demographics
+ where
+  c.c_current_addr_sk = ca.ca_address_sk and
+  ca_state in ('CO','IL','MN') and
+  cd_demo_sk = c.c_current_cdemo_sk 
  group by cd_gender,
           cd_marital_status,
           cd_education_status,
@@ -45,6 +51,4 @@ select top 100
           cd_education_status,
           cd_purchase_estimate,
           cd_credit_rating
- ;
-
-
+ ;    
